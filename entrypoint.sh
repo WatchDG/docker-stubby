50869992c65bf9067e0445b3cbe2af7547fc088a
WatchDG <baranov@w3m.co>
2026-01-31 18:59:22 +0300

    Enhance entrypoint script and stubby configuration to support dynamic log level. Update stubby.yml to include log level placeholder.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 set -eu
 
 listen_port="${STUBBY_LISTEN_PORT:-53}"
+log_level="${STUBBY_LOG_LEVEL:-GETDNS_LOG_ERR}"
 
-sed "s/__STUBBY_LISTEN_PORT__/${listen_port}/g" \
+sed "s/__STUBBY_LISTEN_PORT__/${listen_port}/g; s/__STUBBY_LOG_LEVEL__/${log_level}/g" \
   /etc/stubby/stubby.yml.template > /etc/stubby/stubby.yml
 
 exec stubby -C /etc/stubby/stubby.yml -l
