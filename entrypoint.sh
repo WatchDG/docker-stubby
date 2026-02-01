cf2a41b5d8223da3a52b4a1b19727cc326672f89
WatchDG <baranov@w3m.co>
2026-02-01 11:12:28 +0300

    Implement dynamic DNS transport list configuration in entrypoint script and update stubby.yml to use placeholder for DNS transport list.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,24 @@ TLS_AUTHENTICATION="${STUBBY__TLS_AUTHENTICATION:-GETDNS_AUTHENTICATION_REQUIRED
 TEMPLATE="/etc/stubby/stubby.yml.template"
 CONF="/etc/stubby/stubby.yml"
 
+DNS_TRANSPORT_LIST=""
+i=0
+while [ "${i}" -le 99 ]; do
+  val=""
+  eval "val=\${STUBBY__DNS_TRANSPORT_LIST__${i}-}"
+  if [ "${i}" -eq 0 ] && [ -z "${val}" ]; then
+    val="GETDNS_TRANSPORT_TLS"
+  fi
+  if [ -n "${val}" ]; then
+    if [ -z "${DNS_TRANSPORT_LIST}" ]; then
+      DNS_TRANSPORT_LIST="  - ${val}"
+    else
+      DNS_TRANSPORT_LIST="${DNS_TRANSPORT_LIST}\n  - ${val}"
+    fi
+  fi
+  i=$((i + 1))
+done
+
 sed \
   -e "s/__LISTEN_PORT__/${LISTEN_PORT}/g" \
   -e "s/__LOG_LEVEL__/${LOG_LEVEL}/g" \
@@ -17,6 +35,7 @@ sed \
   -e "s/__EDNS_CLIENT_SUBNET_PRIVATE__/${EDNS_CLIENT_SUBNET_PRIVATE}/g" \
   -e "s/__ROUND_ROBIN_UPSTREAMS__/${ROUND_ROBIN_UPSTREAMS}/g" \
   -e "s/__TLS_AUTHENTICATION__/${TLS_AUTHENTICATION}/g" \
-  "${TEMPLATE}" > "${CONF}"
+  "${TEMPLATE}" | awk -v list="${DNS_TRANSPORT_LIST}" \
+  '{ if ($0 ~ /__DNS_TRANSPORT_LIST__/) { print list; next } print }' > "${CONF}"
 
 exec stubby -C "${CONF}" -l
